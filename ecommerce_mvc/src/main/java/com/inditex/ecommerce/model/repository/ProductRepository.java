58bd27d705b81be464c214ba0bd600880162a626
jcagigas <jose.maria.cagigas@gmail.com>
2023-05-02 14:55:11 +0200

    fix jpa

--- ecommerce_mvc/src/main/java/com/inditex/ecommerce/model/repository/ProductRepository.java
+++ ecommerce_mvc/src/main/java/com/inditex/ecommerce/model/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.inditex.ecommerce.model.repository;
 
 import com.inditex.ecommerce.model.entity.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +9,7 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
-public interface ProductRepository {
+public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
     @Query(value = "SELECT p FROM PRODUCT_ENTITY p " +
             "WHERE (p.PRODUCT_ID =?1 AND p.BRAND_ENTITY_ID =?2 AND ?3 BETWEEN p.START_DATE AND p.END_DATE)" +
             " ORDER BY p.PRIORITY DESC LIMIT 1;")
